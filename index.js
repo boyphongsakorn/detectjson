40f5694fee3e4ff34c2db5f0d8eea17e813c0a80
Phongsakorn Wisetthon <boy1556@hotmail.com>
2022-02-12 19:29:46 +0700

    test

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ client.once('ready', () => {
 
 //get user in voice channel
 //cron.schedule('*/10 19 * * *', async () => {
-var job = new CronJob('*/10 19 * * *', function() {
+var job = new CronJob('*/10 19 * * *', async function() {
     let guilds = client.guilds.fetch('309312041632661504');
     let memberarrray = [];
     //console.log(guilds);
